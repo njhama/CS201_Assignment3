91ef342e824ff3fce21b3d592b4d10134f70f06f
Nicholas Hama <njhama@usc.edu>
2023-10-24 11:37:32 -0700

    connection thread

---
+++ src/ConnectionThread.java
@@ -0,0 +1,50 @@
+import java.net.Socket;
+import java.io.*;
+import java.net.*;
+
+public class ConnectionThread extends Thread{
+	
+	
+	
+	
+	//obj to hold socket, client id
+	
+	//run
+	//send to client outputstream
+	//receive from client inputstream
+	
+	private Socket socket;
+	private int ClientId;
+	ObjectInputStream in;
+	ObjectOutputStream out;
+
+	public ConnectionThread(Socket socket, int ClientID) {
+		this.socket = socket;
+		this.ClientId = ClientID;
+	}
+	
+	
+	public void run() {
+		try {
+			in = new ObjectInputStream(socket.getInputStream());
+			out = new ObjectOutputStream(socket.getOutputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+	
+	 public void sendMessage(String message) {
+	        try {
+	            if (out != null) {
+	                out.writeObject(message);
+	                out.flush();  
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+}
